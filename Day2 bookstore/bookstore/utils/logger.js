573652b34b76d05d19552146e8579d362756bacd
gayathri1525 <srijasuggala20@gmail.com>
2025-11-24 21:58:00 +0530

    Update logger.js

--- Day2 bookstore/bookstore/utils/logger.js
+++ Day2 bookstore/bookstore/utils/logger.js
@@ -1,1 +1,24 @@
 
+const ts = () => new Date().toISOString();
+
+function info(msg, obj) {
+  if (obj) {
+    console.log(`✔ ${msg}`, obj);
+  } else {
+    console.log(`✔ ${msg}`);
+  }
+}
+
+function error(msg, err) {
+  if (err) {
+    console.error(`❌ ${msg}`, err.message ?? err);
+  } else {
+    console.error(`❌ ${msg}`);
+  }
+}
+
+function debug(msg, obj) {
+  console.log(`[${ts()}] ${msg}`, obj ?? "");
+}
+
+module.exports = { info, error, debug };
