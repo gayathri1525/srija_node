51bb45c8cd517bc1dfd02979a5dc33124827ae02
gayathri1525 <srijasuggala20@gmail.com>
2025-11-24 21:56:17 +0530

    Update recommend.js

--- Day2 bookstore/bookstore/recommend.js
+++ Day2 bookstore/bookstore/recommend.js
@@ -1,1 +1,11 @@
+function recommendBooks(userId, alreadyInCartTitles) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const pool = ["You Don't Know JS", "Async JavaScript", "Clean Code"];
+      const recs = pool.filter((t) => !alreadyInCartTitles.includes(t));
+      resolve(recs.slice(0, 2));
+    }, 200);
+  });
+}
 
+module.exports = { recommendBooks };
