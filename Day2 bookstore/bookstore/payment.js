3f9f90d4707a0593847576668d68f99db199fc78
gayathri1525 <srijasuggala20@gmail.com>
2025-11-24 21:53:11 +0530

    Update payment.js

--- Day2 bookstore/bookstore/payment.js
+++ Day2 bookstore/bookstore/payment.js
@@ -1,1 +1,14 @@
+function processPayment(amount) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (amount > 0) {
+                resolve("Payment successful");
+            } else {
+                reject("Payment failed");
+            }
+        }, 1000);
+    });
+}
+
+module.exports = processPayment;
 
