f6c06d9c63909bcddf9024e0aa6199091c90498f
gayathri1525 <srijasuggala20@gmail.com>
2025-11-24 20:39:28 +0530

    Export modules from user, billing, and config files

--- casestudy1/modules/index.js
+++ casestudy1/modules/index.js
@@ -1,1 +1,3 @@
-
+export { User, users } from "./user.js";
+export { calculateBill, billingSummary } from "./billing.js";
+export { PORT, MAX_FILE_SIZE, PRICE_PER_KB, KB_BASE, UPLOAD_DIR } from "./config.js";
