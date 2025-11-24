d06bff7432ab335ac6b0fb885e679227be9c931f
gayathri1525 <srijasuggala20@gmail.com>
2025-11-24 21:51:45 +0530

    Update cart.js

--- Day2 bookstore/bookstore/cart.js
+++ Day2 bookstore/bookstore/cart.js
@@ -1,1 +1,9 @@
+function getUserCart(userId) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(["JS101", "NODE201"]);
+    }, 200);
+  });
+}
 
+module.exports = { getUserCart };
