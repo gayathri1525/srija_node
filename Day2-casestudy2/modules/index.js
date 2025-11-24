3bc9c3468d307b629a09dbdef59090e014c9a345
gayathri1525 <srijasuggala20@gmail.com>
2025-11-24 21:11:18 +0530

    Update index.js

--- Day2-casestudy2/modules/index.js
+++ Day2-casestudy2/modules/index.js
@@ -1,1 +1,3 @@
-
+export { PORT, MAX_MESSAGE_LENGTH, DEFAULT_ROOM } from "./config.js";
+export { User, users, registerUser, findUserByName } from "./user.js";
+export { ChatRoom, rooms, getOrCreateRoom } from "./chatRoom.js";
