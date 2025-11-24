3e49d81ffb82bf59352bdd411b329cc32631cb9e
gayathri1525 <srijasuggala20@gmail.com>
2025-11-24 21:48:01 +0530

    Update auth.js

--- Day2 bookstore/auth.js
+++ Day2 bookstore/auth.js
@@ -1,1 +1,16 @@
+function validateLogin(username, password) {
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      
+      if (username === 'user' && password === 'pass') {
+        resolve({ userId: 101, email: 'sri@example.com' });
+      } else {
+        reject(new Error('Invalid credentials'));
+      }
+    }, 100);
+  });
+}
+
+module.exports = { validateLogin };
 
