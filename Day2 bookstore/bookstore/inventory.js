ca2b49fffa0baebae0285f0634c4b3635dfe62c3
gayathri1525 <srijasuggala20@gmail.com>
2025-11-24 21:52:34 +0530

    Update inventory.js

--- Day2 bookstore/bookstore/inventory.js
+++ Day2 bookstore/bookstore/inventory.js
@@ -1,1 +1,14 @@
+unction checkStock(books) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let outOfStock = false;
+            if (outOfStock) {
+                resolve("Out of stock");
+            } else {
+                resolve("In stock");
+            }
+        }, 1000);
+    });
+}
 
+module.exports = checkStock;
